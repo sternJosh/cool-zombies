28d6e6b00c0f20593018010265cef32b1ed42a96
Joshua Bush <bush@ufl.edu>
2013-05-26 20:57:49 -0400

    first little bit of the character class

---
+++ Assets/Standard Assets/Scripts/New Scripts/CharacterClass.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using System.Collections;
+
+public class CharacterClass : MonoBehaviour {
+	
+	//hit points and vit points
+	private int hitPoints;
+	private int vitPoints;
+	
+	//attributes
+	
+	private int intel;
+	private int composure;
+	private int dex;
+	private int con;
+	private int perc;
+	private int str;
+	
+	//skills
+	private int longGun;
+	private int handGun;
+	private int bows;
+	private int climb;
+	private int acrobatics;
+	//etc...
+	
+	//feats
+	private bool fastMove; //for example...
+	private bool headHunter;
